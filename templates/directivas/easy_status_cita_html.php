f249185459e980ed8aee68f75baed9ca42d152c8
MariNavRam <marisol.navarrete7500@alumnos.udg.mx>
2023-02-08 21:43:52 -0600

    v1.9.0 Crud easy-status-cita

--- templates/directivas/easy_status_cita_html.php
+++ templates/directivas/easy_status_cita_html.php
@@ -1,7 +1,7 @@
 <?php
 namespace html;
 
-use gamboamartin\easybot\models\easy_status_cita;
+use gamboamartin\easybot\models\easy_horario;
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
 use PDO;
@@ -9,12 +9,12 @@ use stdClass;
 
 class easy_status_cita_html extends html_controler {
 
-    public function select_easy_dia_semana_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
+    public function select_easy_horario_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
     {
-        $modelo = new easy_status_cita($link);
+        $modelo = new easy_horario($link);
 
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'Dia Semana',required: true);
+            modelo: $modelo,label: 'Horario',required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
