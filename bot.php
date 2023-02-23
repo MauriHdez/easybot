acf7b2e8e659a18e614d0e895d799bbf99cc9703
FabrizioAvelar <ivan_avelar6@hotmail.com>
2023-02-23 00:35:32 -0600

    v1.13.3 Se ajusta input respuesta bot

--- bot.php
+++ bot.php
@@ -5,8 +5,9 @@ use gamboamartin\easybot\models\bot;
 
 $token = '5423352510:AAHJ86F7ru7OZHXG0E4joj89ji4DmZdMZFI';
 $website = 'https://api.telegram.org/bot'.$token;
+$input = file_get_contents('php://input');
 
-$bot = new bot(token: $token, website: $website);
+$bot = new bot(token: $token, website: $website, input: $input);
 
 
 $datos = $bot->get();
