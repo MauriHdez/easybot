2236d6f43b0859dba8196260b16e88fddea0455b
FabrizioAvelar <ivan_avelar6@hotmail.com>
2023-02-23 00:13:55 -0600

    v1.12.1 Se integra archivo bot

---
+++ bot.php
@@ -0,0 +1,33 @@
+<?php
+$token = '5423352510:AAHJ86F7ru7OZHXG0E4joj89ji4DmZdMZFI';
+$website = 'https://api.telegram.org/bot'.$token;
+
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+var_dump($input);
+
+switch($message) {
+    case '/start':
+        $response = 'Me has iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'Hola! Soy @trecno_bot';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = 'No te he entendido';
+        sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+?>
