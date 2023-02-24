4253d56d2bf14aace83e407a0eb53aeb6f76adfc
FabrizioAvelar <ivan_avelar6@hotmail.com>
2023-02-23 23:57:45 -0600

    1.14.3 Se integra curl agente

--- bot.php
+++ bot.php
@@ -9,7 +9,6 @@ $update = json_decode($input, TRUE);
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 
-var_dump($input);
 
 switch($message) {
     case '/start':
@@ -21,7 +20,7 @@ switch($message) {
         sendMessage($chatId, $response);
         break;
     default:
-        $response = $input;
+        $response = getResponse(message: $message);
         sendMessage($chatId, $response);
         break;
 }
@@ -30,4 +29,30 @@ function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
 }
+
+function getResponse($message){
+    $json = '{
+          "queryInput": {
+            "text": {
+              "text": "'. $message .'"
+            },
+            "languageCode": "en"
+          },
+          "queryParams": {
+            "timeZone": "America/Los_Angeles"
+          }
+        }';
+
+    $ch = curl_init('https://us-central1-dialogflow.googleapis.com/v3/projects/easyacces-378204/locations/us-central1/agents/0e785973-9af6-431b-bbc6-e5b351364eb8/sessions/SESSION_ID:detectIntent');
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer $(gcloud auth print-access-token)', 'x-goog-user-project: easyacces-378204','Content-Type: application/json; charset=utf-8', ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
 ?>
