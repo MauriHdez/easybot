b5b5a1ab1e9f487be63129c2881693f4b1e00bda
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-03-16 20:09:19 -0600

    v1.19.3 Se webhook

--- webhook.php
+++ webhook.php
@@ -1,13 +1,13 @@
 <?php
 function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
+    //if($update["result"]["action"] == "sayHello"){
         sendMessage(array(
             "source" => $update["result"]["source"],
             "speech" => "Hello from webhook",
             "displayText" => "Hello from webhook",
             "contextOut" => array()
         ));
-    }
+    //}
 }
 
 function sendMessage($parameters) {
@@ -16,6 +16,6 @@ function sendMessage($parameters) {
 
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
+//if (isset($update["result"]["action"])) {
     processMessage($update);
-}
+//}
