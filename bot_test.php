e14691f3125706110242e60efdb33435a47c80b7
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-03-17 09:38:49 -0600

    v1.20.3 Se integra bot test

---
+++ bot_test.php
@@ -0,0 +1,65 @@
+<?php
+
+
+use config\generales;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = '11111';
+$message = 'hola este es el mensaje de usuario';
+
+
+switch($message) {
+    case '/start':
+        $response = 'Me has iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'Hola! Soy @trecno_bot';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = getResponse(message: $message, chatId: $chatId);
+        echo $response;exit;
+        $response = json_decode($response)->queryResult->responseMessages[0]->text->text[0];
+        //sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
+
+
+function getResponse($message, $chatId){
+    $generales = new generales();
+    $json = '{
+          "queryInput": {
+            "text": {
+              "text": "'. $message .'"
+            },
+            "languageCode": "en"
+          },
+          "queryParams": {
+            "timeZone": "America/Los_Angeles",
+            "webhookHeaders": {
+                "chatid": "'. $chatId .'"
+            }
+          }
+        }';
+
+    $ch = curl_init('https://us-central1-dialogflow.googleapis.com/v3/projects/easyacces-378204/locations/us-central1/agents/0e785973-9af6-431b-bbc6-e5b351364eb8/sessions/SESSION_ID:detectIntent');
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ya29.a0AVvZVsrbxw2AkYJmc9Ksrfon24-NeBjR7STeDkMvNcVEOUnp9ST8OJqG9XJsCJTwfFu37oQv6fJmd3WYX2remY8yiDXd5TLzccqE8b8XvTf5ZP41SOQ461PDTeMAzJiu8j1yX2GB-oGelEOIcJQYeArRKubZ7WF1I7ngjgaCgYKAcYSAQASFQGbdwaIMJQ8Gu-VnzhvIkllErZK_g0173', 'x-goog-user-project: easyacces-378204','Content-Type: application/json; charset=utf-8', ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
+?>
