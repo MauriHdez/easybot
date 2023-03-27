682676c601c7b336dd413df42f46a4a356398ac9
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-03-27 13:17:27 -0600

    v1.25.3 Se ajusta archivo bot

--- bot.php
+++ bot.php
@@ -25,10 +25,10 @@ $update = json_decode($input, TRUE);
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 
-
+/*
 $chatId = '5655914615';
 $message = 'Mauricio';
-
+*/
 
 switch($message) {
     case '/start':
