26e89a97bb4063755780689f344e65e6f850c123
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-05-03 23:51:23 -0600

    v1.34.3 Se ajusta cancela cita

--- bot.php
+++ bot.php
@@ -27,10 +27,10 @@ $update = json_decode($input, TRUE);
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 
-
+/*
 $chatId = '5655914615';
 $message = 'cuando es mi cita?';
-
+*/
 switch($message) {
     case '/start':
         $response = 'Me has iniciado';
